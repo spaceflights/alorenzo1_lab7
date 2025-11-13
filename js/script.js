70bf1b4e0ea53b4b844cd7846b6911dae0a4bf72
Ana Lorenzo <ana.lorenzo77@hotmail.com>
2025-11-12 18:31:39 -0600

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-function init(){
-//add your javascrip between these two lines of code
- 
+function init() {
+    let button = document.getElementById("entrybutton");
 
-
-
-
-
-
+    button.addEventListener("click", function() {
+        let userText = document.getElementById("entryinput").value;
+        alert(userText);
+        document.getElementById("textoutput").textContent = userText;
+    });
+}
 
 window.addEventListener('load', init);
